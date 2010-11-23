de618c8e663b37b69511dbe73131f46a050a9e48
drakaz <drakpub@gmail.com>
2010-11-23 16:46:03 +0100

    Revert "Battery temp : increase kernel return (*10) to fit android expectation" This will cause bootloop on some device which report temperature > 90
    This reverts commit 7505249cea90dfaa3467f5e2d2125d4fcac318e3.

--- services/java/com/android/server/BatteryService.java
+++ services/java/com/android/server/BatteryService.java
@@ -212,9 +212,6 @@ class BatteryService extends Binder {
 
         shutdownIfNoPower();
         shutdownIfOverTemp();
-        
-        // drakaz : increase kernel return (*10) to fit android expectation
-        mBatteryTemperature = mBatteryTemperature * 10;
 
         mBatteryLevelCritical = mBatteryLevel <= CRITICAL_BATTERY_LEVEL;
         if (mAcOnline) {
